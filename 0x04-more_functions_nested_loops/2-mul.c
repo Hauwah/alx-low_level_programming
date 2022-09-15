0789414784043678a557eed162f04976a2dca1b1
hauwa <timilehin312@gmail.com>
2022-09-15 11:51:15 -0700

    My commit 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * mul -multiplies two integers
+ *mul - multiplies two integers
  *@a: print int
  *@b: print int 
- * Return: Always 0.
+ *Return: Always 0.
  */
 
 int mul(int a, int b)
