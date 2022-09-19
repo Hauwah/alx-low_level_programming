1ac91a79dac0fa13095009aa695f4371111bc298
hauwa <timilehin312@gmail.com>
2022-09-19 16:50:03 -0700

    My 11th task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * my task
  */
 int main(void)
 {
 int num;
 
-srand(time(0));
+srand (time(0));
 num = rand();
-printf("%j\n", num);
+printf("%i\n", num);
 return (0);
 }
