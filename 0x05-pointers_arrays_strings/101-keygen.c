7a1557eb834363c877002dbf35e347155934e16a
hauwa <timilehin312@gmail.com>
2022-09-19 16:45:03 -0700

    My 11th task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * my task
+ */
+int main(void)
+{
+int num;
+
+srand(time(0));
+num = rand();
+printf("%j\n", num);
+return (0);
+}
