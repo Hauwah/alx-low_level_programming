4c07467878bd2b452cc8b02ae083cc28103fbe24
hauwa <timilehin312@gmail.com>
2022-09-26 20:30:33 -0700

    My task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strspn - locates a character in a string
+ * _strspn - Locates a character in a string
  * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s 
+ * @accept: This is the string containing the list of characters to match in s
  * Return: return count
- */
+ **/
 
 unsigned int _strspn(char *s, char *accept)
 
@@ -19,26 +19,26 @@ unsigned int _strspn(char *s, char *accept)
 
           i = 0;
           while (str1[i] != '\0') /*Declaring WHILE *s */
-	  {
-		  j = 0;
-		  while (str2[j] != '\0') /*Declaring WHILE *accept*/
+          {
+                  j = 0;
+                  while (str2[j] != '\0') /*Declaring WHILE *accept*/
                   {
-			  if (str2[j] == str1[i] /*Evaluate condition*/
+                          if (str2[j] == str1[i]) /*Evaluate condition*/
                           {
-                		  count++; /*count number*/	
-		                  break;
-		          }
+                                  count++; /*count number*/
+                                  break;
+                          }
 
-	                  j++;    /*add j+1*/
-	          } 
+                          j++;    /*add j+1*/
+                  }
 
-                  if (s[j] != accept[j]) /*If aren't equals*/
+                  if (s[i] != accept[j]) /*If aren't equals*/
                   {
                           break;
-	          } 
+                  }
 
                   i++; /*add x+1*/
-           }
+          }
 
-           return (count); /*return the value of count*/
+          return (count); /*return the value of count*/
 }
