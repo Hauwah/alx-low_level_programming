44f1bba551423a0d6f1c925604357efa852ae1e5
Hauwah <Timilehin312@gmail.com>
2022-09-28 07:07:02 -0700

    My task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 
 int _strlen_recursion(char *s)
-{
 
+{
 /*Base condition*/
 if (*s == '\0')
 return (0);
 
 else
-return (1 + _strlen_recursion(s + 1)); /*sum1*/
+return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
 }
